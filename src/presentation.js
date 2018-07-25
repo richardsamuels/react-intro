49a0358e4c93457baaba7978a5929876d4f9becc
Richard Samuels <richard.samuels@mongodb.com>
2018-07-24 21:54:45 -0400

    history lesson done

--- src/presentation.js
+++ src/presentation.js
@@ -45,9 +45,9 @@ const yeoldejs = `function fetchData(page) {
   $.get('http://my.domain/api/data', {page: page}, function(resp) {
       $("#fetch_button").remove();
       for (var i = 0; i < resp.length; ++i) {
-          $("#id").append("<div>" + resp[i] + "</div>");
-          $("#id").append('<input id="fetch_button" type="button" onClick="fetchData(' + (page + 1) + ')">Next</input>');
+          $("#id").append("<div>" + resp[i] + "</div");
       }
+      $("#id").append('<input id="fetch_button" type="button" onClick="fetchData(' + (page + 1) + ')">Next</input>');
   });
 }`
 
@@ -74,13 +74,14 @@ export default class Presentation extends React.Component {
           <Notes>
             <h4>Slide notes</h4>
             <ol>
-              <li>started ~12</li>
-              <li>really obsessed with login forms</li>
-              <li>LAMP stack</li>
+              <li>I started ~12</li>
+              <li>I was really obsessed with how login forms worked</li>
+              <li>I wrote atrotious code, even by PHP 4 standards</li>
+              <li>era of the LAMP stack</li>
             </ol>
           </Notes>
           <Heading size={1} textColor="secondary">
-            About me + History
+            about me + history
           </Heading>
         </Slide>
         <Slide transition={['fade']} bgColor="tertiary">
@@ -92,25 +93,25 @@ export default class Presentation extends React.Component {
             </ol>
           </Notes>
           <Heading size={1} textColor="secondary">
-            Web Dev circa 2008
+            web dev circa 2008
           </Heading>
           <Appear>
-            <Text margin="10px 0 0" textColor="primary" textSize={'1.2em'}>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
               IE, Firefox 3.x, Mozilla, Chrome's first public release
             </Text>
           </Appear>
           <Appear>
-            <Text margin="10px 0 0" textColor="primary" textSize={'1.2em'}>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
               People cared about Opera
             </Text>
           </Appear>
           <Appear>
             <Text margin="10px 0 0" textColor="primary" textSize={'0.8em'} italic>
-              No one cared about Safari
+              People were starting to care about Safari (iPhone released in 2007)
             </Text>
           </Appear>
           <Appear>
-            <Text margin="10px 0 0" textColor="primary" textSize={'1.2em'}>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
               Node.js literally didn't exist yet (May 2009)
             </Text>
           </Appear>
@@ -120,13 +121,13 @@ export default class Presentation extends React.Component {
             </Text>
           </Appear>
           <Appear>
-            <Text margin="10px 0 0" textColor="primary" textSize={'1.2em'}>
-              Internet Explorer 6 received its last update
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              You could turn Javascript off and use the web
             </Text>
           </Appear>
           <Appear>
-            <Text margin="10px 0 0" textColor="primary" textSize={'1.2em'}>
-              You could turn Javascript off and use the web
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              Internet Explorer 6 received its last update
             </Text>
           </Appear>
           {/*
@@ -154,9 +155,77 @@ export default class Presentation extends React.Component {
           <Notes>
             <h4>Slide notes</h4>
             <ol>
+              <li>Microsoft proprietary stuff</li>
+              <li>ActiveX loaded + executed arbitrary code from websites</li>
+              <li>Conditional HTML execution</li>
+              <li>Microsoft didn't care about the security of your system</li>
+              <li>Microsoft didn't care about the HTML standard</li>
+              <li>Microsoft didn't care about the CSS standard</li>
+              <li>Microsoft didn't care about the Javascript standard</li>
+            </ol>
+          </Notes>
+          <Heading size={1} textColor="secondary">
+            internet explorer 6 + micros*ft
+          </Heading>
+          <Appear>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              Microsoft didn't care about the security of your system
+            </Text>
+          </Appear>
+          <Appear>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              Microsoft didn't care about the HTML standard
+            </Text>
+          </Appear>
+          <Appear>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              Microsoft didn't care about the CSS standard
+            </Text>
+          </Appear>
+          <Appear>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              Microsoft didn't care about the Javascript standard
+            </Text>
+          </Appear>
+          <Appear>
+            <Text margin="30px 0 0" textColor="primary" textSize={'1.2em'}>
+              Microsoft didn't care about compatibility
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Notes>
+            <h4>Slide notes</h4>
+            <ol>
+              <li>Macromedia was originally indepedent and later acquired by Adobe</li>
+              <li>Proprietary (hundreds a developer seat) and big files (megabytes)</li>
+              <li>Windows + Mac OS X</li>
+            </ol>
+          </Notes>
+          <Heading size={1} textColor="secondary">
+            <Image src="macromedia-shockwave-enabled-1.svg" width={'31%'} />
+            <Image src="macromedia-flash-enabled-1.svg" width={'31%'} />
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Notes>
+            <h4>Slide notes</h4>
+            <ol>
+              <li>Release of jQuery</li>
+            </ol>
+          </Notes>
+          <Heading size={1} textColor="secondary">
+        2006
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Notes>
+            <h4>Slide notes</h4>
+            <ol>
               <li>The role jQuery played in changing front-end dev cannot be understated.</li>
               <li>jQuery was one of the first polyfills/modernizers/sane-ifiers</li>
               <li>jQuery made it possible to have confidence that the code you wrote would work in Internet Explorer, Mozilla Internet Suite, Netscape</li>
+              <li>jQuery was free. Macromedia (now Adobe) Flash</li>
             </ol>
           </Notes>
           <Heading size={1} textColor="secondary">
@@ -169,13 +238,14 @@ export default class Presentation extends React.Component {
           lang="js"
           code={yeoldejs}
           ranges={[
-            { loc: [0, 9], title: "Ye Olde Days" },
-            { loc: [1, 2], title: "Fetching data" },
+            { loc: [0, 9], title: "ye olde days" },
+            { loc: [1, 2], title: "fetching data" },
             { loc: [3, 4], title: "iterate" },
             { loc: [3, 6], title: "insert" },
             { loc: [0, 9], title: "it works" },
+            { loc: [4, 5], title: "haha no", note: "unclosed div. good luck" },
             // ...
-          ]}/>
+          ]} />
         <Slide transition={['fade']} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Some Pretentious Bullshit</Quote>
